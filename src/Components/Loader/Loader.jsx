40bb24552a59c3a1c19e384985d5563698ef18f2
Samuel Narh <samuelnarh38@gmail.com>
2024-05-02 15:03:05 +0000

    update loader

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -3,27 +3,18 @@ import "./Loader.css";
 
 export const Loader = () => {
   return (
-    <div
-      aria-label="Orange and tan hamster running in a metal wheel"
-      role="img"
-      className="wheel-and-hamster flex justify-center"
-    >
-      <div className="wheel"></div>
-      <div className="hamster">
-        <div className="hamster__body">
-          <div className="hamster__head">
-            <div className="hamster__ear"></div>
-            <div className="hamster__eye"></div>
-            <div className="hamster__nose"></div>
-          </div>
-          <div className="hamster__limb hamster__limb--fr"></div>
-          <div className="hamster__limb hamster__limb--fl"></div>
-          <div className="hamster__limb hamster__limb--br"></div>
-          <div className="hamster__limb hamster__limb--bl"></div>
-          <div className="hamster__tail"></div>
-        </div>
+    <div className="flex-col gap-4 w-full flex items-center justify-center">
+      <div className="w-28 h-28 border-8 text-blue-400 text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-blue-400 rounded-full">
+        <svg
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          height="1em"
+          width="1em"
+          className="animate-ping"
+        >
+          <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z"></path>
+        </svg>
       </div>
-      <div className="spoke"></div>
     </div>
   );
 };
